9b8f90e8a4eb3c3a93fe53700cd01d51da7fab60
b_rasulov <fattoyeva_n@nuu.uz>
2026-01-28 14:38:33 +0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ShopPage } from './pages/ShopPage';
 import { ProductDetailPage } from './pages/ProductDetailPage';
 import { CartProvider } from './hooks/useCart';
 export function App() {
-  return <CartProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<ShopPage />} />
-          <Route path="/product/:id" element={<ProductDetailPage />} />
-        </Routes>
-      </Router>
-    </CartProvider>;
+	useEffect(() => {
+		const tg = (window as any).Telegram?.WebApp;
+		if (tg) {
+			tg.ready();
+			tg.expand();
+		}
+	}, []);
+	return (
+		<CartProvider>
+			<Router>
+				<Routes>
+					<Route path='/' element={<ShopPage />} />
+					<Route path='/product/:id' element={<ProductDetailPage />} />
+				</Routes>
+			</Router>
+		</CartProvider>
+	);
 }
